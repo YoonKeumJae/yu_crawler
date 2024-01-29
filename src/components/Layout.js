6ba3e774c83570033a28d224f2248852886dbe58
KimSound424 <ykj1512@gmail.com>
2024-01-29 16:06:36 +0900

    [Feat] Logout 기능 추가

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,29 @@
+import { signOut } from "firebase/auth";
+import styled from "styled-components";
+import { auth } from "../firebase";
+import { Outlet, useNavigate } from "react-router-dom";
+
+const Wrapper = styled.div``;
+const Logout = styled.button``;
+
 const Layout = () => {
-    return (
-        <div>
-            Layout here            
-        </div>
-    );
+  const navigate = useNavigate();
+
+  const onClickLogout = async () => {
+    const ok = window.confirm("Are you sure you want to log out?");
+    if (ok) {
+      await signOut(auth);
+      navigate("/signin");
+    }
+  };
+
+  return (
+    <Wrapper>
+      Layout here
+      <Logout onClick={onClickLogout}>Log out</Logout>
+      <Outlet />
+    </Wrapper>
+  );
 };
 
 export default Layout;
