a79c2d98c8ef91e6c8ff4f9c15262d5f6448eeb5
KimSound424 <ykj1512@gmail.com>
2024-01-31 17:02:39 +0900

    [Feat] Login page style

---
+++ src/styles/components/StyledGoogleBtn.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+    margin-top: 10px;
+    background-color: #fff;
+    border: 1px solid #000;
+    border-radius: 5px;
+    cursor: pointer;
+`;
+
+export default Button;
