8552a917573d8b7aceb8b54b45d457394da17605
KimSound424 <ykj1512@gmail.com>
2024-02-01 20:08:55 +0900

    [Feat] 구독 항목 추가, 제거

---
+++ src/components/SourceAdd.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div``;
+const Plus = styled.button``;
+const Title = styled.span``;
+
+const SourceAdd = () => {
+  return (
+    <Wrapper>
+      <Plus>+</Plus>
+      <Title>출처 이름</Title>
+    </Wrapper>
+  );
+};
+
+export default SourceAdd;
