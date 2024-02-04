76eefa3e09777b69f9ad39664f01544421018d7d
KimSound424 <ykj1512@gmail.com>
2024-02-04 18:51:15 +0900

    [Deploy] Deploy for testing api

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,18 @@
+import { useEffect } from "react";
 import ListBox from "../components/ListBox";
 import { Wrapper, Title } from "../styles/routes/StyledHome";
+import axios from "axios";
 const Home = () => {
+  useEffect(() => {
+    axios
+      .get("https://yucrawlerserver.web.app/yuhome")
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
     <Wrapper>
       <Title>Title</Title>
