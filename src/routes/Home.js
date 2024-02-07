bcb86bbe8b40bb87db5cfe481f4f70893178446b
KimSound424 <ykj1512@gmail.com>
2024-02-07 15:42:56 +0900

    [Fix] test code deleted

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,18 +1,7 @@
-import { useEffect } from "react";
 import ListBox from "../components/ListBox";
 import { Wrapper, Title } from "../styles/routes/StyledHome";
-import axios from "axios";
 const Home = () => {
-  useEffect(() => {
-    axios
-      .get("https://yucrawlerserver.web.app/yuhome")
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
+  
   return (
     <Wrapper>
       <Title>Title</Title>
