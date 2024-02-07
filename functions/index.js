700f82730e8390b5245e344e86c77f21519a2bf4
KimSound424 <ykj1512@gmail.com>
2024-02-07 16:04:38 +0900

    [Test] Example api

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,10 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const functions = require("firebase-functions");
+const express = require("express");
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+const app = express();
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+app.get("/api/data", (req, res) => {
+  res.send("This is data from Firebase Functions using Express!");
+});
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.api = functions.https.onRequest(app);
