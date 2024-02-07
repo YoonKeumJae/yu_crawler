2b520440d391bb86493ec431f9ee92659b6b4f43
KimSound424 <ykj1512@gmail.com>
2024-02-07 16:31:26 +0900

    .

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,20 @@
+import axios from "axios";
 import ListBox from "../components/ListBox";
 import { Wrapper, Title } from "../styles/routes/StyledHome";
+import { useEffect } from "react";
+
 const Home = () => {
-  
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const data = axios.get("/api/data");
+        console.log(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getData();
+  }, []);
   return (
     <Wrapper>
       <Title>Title</Title>
