e3ce066f04ed0bab0f84ea2aa92b11fb338d3ff9
KimSound424 <ykj1512@gmail.com>
2024-02-08 15:20:34 +0900

    [Feat] express 연동 test

--- functions/index.js
+++ functions/index.js
@@ -7,15 +7,26 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const { onRequest } = require("firebase-functions/v2/https");
+// const { onRequest } = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 const { setGlobalOptions } = require("firebase-functions/v2");
+const functions = require("firebase-functions");
+const express = require("express");
 setGlobalOptions({ maxInstances: 10 });
+
+const app = express();
+
+app.get("/hello", (req, res) => {
+  logger.info("Hello logs!", { structuredData: true });
+  res.json({ message: "Hello from Firebase!" });
+});
+
+exports.api = functions.https.onRequest(app);
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
-exports.helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase!");
-});
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", { structuredData: true });
+//   response.send("Hello from Firebase!");
+// });
